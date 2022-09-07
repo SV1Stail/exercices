4fbfe72cd91a92906fc50d0f1c55a5c3c07ac63b
SV1Stail <vprs6900@mail.ru>
2022-09-07 10:30:13 +0300

    работа с if

---
+++ ex1.py
@@ -0,0 +1,28 @@
+""""
+a = int(input())
+#a=a**2
+#print(a)
+#a=a**3
+#print(a)
+
+
+if a>0 :
+    print("положительное")
+elif a<0:
+    print("отрицательное")
+
+"""
+
+a,b,c=input("вводить без пробелов: ")
+
+
+a=int(a)
+b=int(b)
+c=int(c)
+
+z=0
+
+if (a<b) and (b<c):
+    print("да")
+else:
+    print("нет")
